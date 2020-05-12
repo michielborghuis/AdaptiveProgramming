9380d0b46ee9d9349177808a5b9f0dc5a1502511
michielborghuis <59872213+michielborghuis@users.noreply.github.com>
2020-05-12 19:02:20 +0200

    commit opdr 2b

--- src/formatieveOpdracht2b/Main.java
+++ src/formatieveOpdracht2b/Main.java
@@ -32,7 +32,36 @@ public class Main {
         FiniteStateMachine f1 = new FiniteStateMachine(n0);
         System.out.println(f1.toNode());
 
-        FiniteStateMachine f2 = new FiniteStateMachine(n0);
-
+        Node nn0 = new Node("s0", false);
+        Node nn1 = new Node("s1", false);
+        Node nn2 = new Node("s2", false);
+        Node nn3 = new Node("s3", false);
+        Node nn4 = new Node("s4", false);
+        Node nn5 = new Node("s5", false);
+        Node nn6 = new Node("s6", false);
+        Node dd1 = new Node("dice1", true);
+        Node dd2 = new Node("dice2", true);
+        Node dd3 = new Node("dice3", true);
+        Node dd4 = new Node("dice4", true);
+        Node dd5 = new Node("dice5", true);
+        Node dd6 = new Node("dice6", true);
+        Node dd7 = new Node("dice7", true);
+        Node dd8 = new Node("dice8", true);
+        nn0.setNextStateA(nn1);
+        nn0.setNextStateB(nn2);
+        nn1.setNextStateA(nn3);
+        nn1.setNextStateB(nn4);
+        nn2.setNextStateA(nn5);
+        nn2.setNextStateB(nn6);
+        nn3.setNextStateA(dd1);
+        nn3.setNextStateB(dd2);
+        nn4.setNextStateA(dd3);
+        nn4.setNextStateB(dd4);
+        nn5.setNextStateA(dd5);
+        nn5.setNextStateB(dd6);
+        nn6.setNextStateA(dd7);
+        nn6.setNextStateB(dd8);
+        FiniteStateMachine f2 = new FiniteStateMachine(nn0);
+        System.out.println(f2.toNode());
     }
 }
